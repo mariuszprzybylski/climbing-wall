325a7a00a186fea013c5ed65340658bea1d766c1
Mariusz <mariusz_przybylski@wp.pl>
2018-07-27 22:28:20 +0200

    first commit

---
+++ src/main/java/pl/mprzybylski/climbingWall/ClimbingWallApplication.java
@@ -0,0 +1,12 @@
+package pl.mprzybylski.climbingWall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingWallApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/mprzybylski/climbingWall/ClimbingWallApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mprzybylski.climbingWall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClimbingWallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
