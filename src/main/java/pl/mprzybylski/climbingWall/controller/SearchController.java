e4b942b63d0c269d19a552d7fdc1c87c6b5ed4cd
Mariusz <mariusz_przybylski@wp.pl>
2018-09-01 00:50:12 +0200

    added search

---
+++ src/main/java/pl/mprzybylski/climbingWall/controller/SearchController.java
@@ -0,0 +1,31 @@
+package pl.mprzybylski.climbingWall.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.mprzybylski.climbingWall.dao.ClimberDao;
+
+@Controller
+public class SearchController {
+
+
+    @Autowired
+    private ClimberDao climberDao;
+
+
+    @GetMapping("/climbers/search")
+    public String search(@RequestParam String name, ModelMap modelMap){
+
+        modelMap.put("climber",climberDao.findByName(name).get());
+        return "climbers/result";
+    }
+
+    @GetMapping("/search")
+    public String display(){
+        return "climbers/search";
+    }
+}
--- src/main/java/pl/mprzybylski/climbingWall/dao/ClimberDao.java
+++ src/main/java/pl/mprzybylski/climbingWall/dao/ClimberDao.java
@@ -3,5 +3,8 @@ package pl.mprzybylski.climbingWall.dao;
 import org.springframework.data.repository.CrudRepository;
 import pl.mprzybylski.climbingWall.model.Climber;
 
+import java.util.Optional;
+
 public interface ClimberDao extends CrudRepository<Climber, Integer> {
+    Optional<Climber> findByName(String name);
 }
